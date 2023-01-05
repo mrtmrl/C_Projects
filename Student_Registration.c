b74a128780e9d5d2133408e7dc8c3abfac4dae1e
Mert Meral <56873212+mrtmrl@users.noreply.github.com>
2023-01-05 23:19:37 +0300

    Update Student_Registration.c

--- Student_Registration.c
+++ Student_Registration.c
@@ -1,17 +1,4 @@
-#include<stdio.h>
-
-struct student
-{
-	char name[20];
-	char surname[20];
-	int id;
-};
-#include<stdio.h>
-
-struct student
-{
-	char name[20];
-	char surname[20];#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -114,44 +101,3 @@ void studentsearch()
 	}
 }
 	
-
-	int id;
-};
-
-
-int main()
-{
-	do
-	{
-	printf("Welcome To Student Registration Sytem\n");
-	printf("Select The Action You Want To Do \n\n");
-	printf("1: New Student Registration\n");
-	printf("2: Student Search\n");
-	printf("3: List All Student\n");
-	printf("4: Exit\n");
-	int selection;
-	scanf("%d", &seleciton);
-	
-	switch(selectin)
-	{
-		case'1':
-			printf("Enter Etudent Name: \n");
-			printf("Enter Student Surname: \n");
-			printf("Enter Student ID");
-		case'2':
-			printf("Enter Student ID");
-		case'3':
-			
-		default:
-			printf("There Is No Such Selection");
-		break;
-	}
-	while(selection!='4');
-	return 0;
-}
-	
-
-int main()
-{
-	
-}

863311c6b680dd882e4f19c92e2def820bb74305
Mert Meral <56873212+mrtmrl@users.noreply.github.com>
2023-02-24 17:47:33 +0300

    Add files via upload

---
+++ Bus Company.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+	int i,j;
+   int arr[12][3];
+   int sayac = 1;
+   for(i=0; i<12; i++) {
+      for(j=0; j<2; j++) {
+         arr[i][j] = sayac++;
+      }
+      arr[i][2] = 'x';
+   }
+
+  
+   int genislik1 = 3;
+   int genislik2 = 1;
+   int genislik3 = 3;
+   int genislik4 = 3;
+
+   
+   for(i=0; i<12; i++) {
+      printf("%*d ", genislik1, arr[i][0]); 
+      printf("%*c", genislik2, arr[i][2]); 
+      printf("%*d ", genislik3, arr[i][1]); 
+      printf("%*d\n", genislik4, arr[i][3]); 
+   }
+   
+   return 0;
+}
+

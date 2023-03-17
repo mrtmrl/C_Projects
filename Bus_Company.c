2c2fb3cd1ff494387a8e737a7f1ed6fd1c37b972
Mert Meral <56873212+mrtmrl@users.noreply.github.com>
2023-03-17 14:59:43 +0300

    Add files via upload

--- Bus_Company.c
+++ Bus_Company.c
@@ -1,27 +1,76 @@
 #include <stdio.h>
 
+struct Bus 
+{
+  int seat_arr[12][4];
+};
+
+// Print seat fonction
+void seat_print(struct Bus bus) 
+{
+  int i,j;
+  for (i = 0; i < 12; i++) 
+  {
+    for (j = 0; j < 4; j++) 
+    {
+      if(j==1)
+      {
+        printf(" %c", bus.seat_arr[i][j]);
+      }
+      else{
+         printf(" %d", bus.seat_arr[i][j]);
+      }
+    }
+    printf("\n");
+  }
+}
+
 int main() 
 {
-   int arr[12][3];
-	int i,j;
-   int sayac = 1;
-   for(i=0; i<13; i++) {
-      for(j=0; j<2; j++) {
-         arr[i][j] = sayac++;
+  struct Bus bus;
+  int count = 1;
+  int i,j;
+  int console;
+  
+  // seat number sorting
+  for (i = 0; i < 12; i++) 
+  {
+    for (j = 0; j < 4; j++) 
+    {
+      if (j == 1) 
+      {
+        bus.seat_arr[i][j] = ' ';
+      } 
+      else 
+      {
+        bus.seat_arr[i][j] = count;
+        count++;
       }
-      arr[i][2] = ' ';
-   }
-   int genislik1 = 3;
-   int genislik2 = 1;
-   int genislik3 = 3;
-   int genislik4 = 3;
+    }
+  }
+  
+   //main console 
+  do
+  {
+    printf("1- Seat Numbers\n");
+    printf("2- Exit\n");
+    printf("Select What You Want : ");
+    scanf("%d", &console);
+    
+    switch(console) {
+      case 1:
+        printf("\nSeats:\n");
+        seat_print(bus);
+		printf("\n");
+        break;
+      case 2:
+        printf("\nExiting The Program...\n");
+        break;
+      default:
+        printf("\nWrong Selection! Please Enter The Correct Selection.\n\n");
+    }
+  } while (console != 2);
 
-   for(i=0; i<12; i++) {
-      printf("%*d ", genislik1, arr[i][0]);
-      printf("%*c", genislik2, arr[i][2]); 
-      printf("%*d ", genislik3, arr[i][1]); 
-      printf("%*d\n", genislik4, arr[i][3]); 
-   }
-   
-   return 0;
+  return 0;
 }
+

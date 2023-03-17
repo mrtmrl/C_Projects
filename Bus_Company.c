7c738a565cf68c72d48f7a65e21935a5dac911e1
Mert Meral <56873212+mrtmrl@users.noreply.github.com>
2023-03-17 16:13:59 +0300

    Add files via upload

--- Bus_Company.c
+++ Bus_Company.c
@@ -17,12 +17,32 @@ void seat_print(struct Bus bus)
       {
         printf(" %c", bus.seat_arr[i][j]);
       }
+      else if(bus.seat_arr[i][j]== 'M' &&  bus.seat_arr[i][j]== 'F')
+      {
+      	printf(" %c", bus.seat_arr[i][j]);
+	  }
       else{
          printf(" %d", bus.seat_arr[i][j]);
       }
     }
     printf("\n");
   }
+}
+
+void seat_choice(struct Bus *bus)
+{
+	int i,j,choice;
+	scanf("%d", &choice);
+		for (i = 0; i < 12; i++) 
+  		{
+   	 		for (j = 0; j < 4; j++) 
+   		 	{
+				if(bus->seat_arr[i][j]==choice)
+				{
+		  			bus->seat_arr[i][j]='999';
+			 	}
+      		}
+    	}
 }
 
 int main() 
@@ -52,25 +72,33 @@ int main()
    //main console 
   do
   {
-    printf("1- Seat Numbers\n");
-    printf("2- Exit\n");
+  	printf("-----------------------------------\n");
+    printf("1- Show The Seat Number List\n");
+    printf("3- Select The Seat Number\n");
+    printf("3- Exit\n");
+    printf("-----------------------------------\n\n");
     printf("Select What You Want : ");
     scanf("%d", &console);
     
-    switch(console) {
+    switch(console) 
+	{
       case 1:
         printf("\nSeats:\n");
         seat_print(bus);
 		printf("\n");
         break;
       case 2:
-        printf("\nExiting The Program...\n");
+      	seat_choice(&bus);
+        printf("\n");
         break;
+      case 3:
+      	printf("\nExiting The Program...\n");
+		break;
       default:
         printf("\nWrong Selection! Please Enter The Correct Selection.\n\n");
     }
-  } while (console != 2);
-
+  }
+  while (console != 3);
+  
   return 0;
 }
-
